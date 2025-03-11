bee5424bc6996b6bc6fa29df8e1e94ae47b0cb48
ychartois <ychartois@gmail.com>
2025-03-11 19:42:41 +0000

    Add old contract + compile

---
+++ contracts/zombieownership.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+
+/// @title ZombieOwnership - ownership of zombies and erc721 token contract
+/// @author yannig <yannig.inscription@gmail.com>
+/// @dev ERC721 contract to be used as a base for the ZombieOwnership contract.
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    mapping (uint => address) zombieApprovals;
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return zombieToOwner[_tokenId];
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+        ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+        zombieApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+
+}
