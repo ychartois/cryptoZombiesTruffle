5122c8bcfcda34b34d8d6a281f47c166941a405b
ychartois <ychartois@gmail.com>
2025-03-14 11:55:05 +0000

    Add first unit test

---
+++ test/CryptoZombies.js
@@ -0,0 +1,11 @@
+const CryptoZombies = artifacts.require("CryptoZombies");
+
+contract("CryptoZombies", (accounts) => {
+    let [alice, bob] = accounts;
+    it("should be able to create a new zombie", async () => {
+        const contractInstance = await CryptoZombies.new();
+        const zombieNames = ["Zombie #1", "Zombie #2"];
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        assert.equal(result.receipt.status, true);
+    })
+})
